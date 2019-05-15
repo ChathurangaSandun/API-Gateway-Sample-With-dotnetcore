33db72ee3f5eeb910472715c03562ff44581eb9e
chathurangasandun <csk@tiqri.com>
2019-05-15 14:11:51 +0300

    finish

---
+++ OcelotApiGateway/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace OcelotApiGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                // Informs the app to use the ocelot.json file
+                config.AddJsonFile("ocelot.json");
+            })
+
+
+            .UseStartup<Startup>();
+    }
+}
